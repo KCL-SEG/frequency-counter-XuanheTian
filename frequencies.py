ddf515faebb3e7e16b0535f7ddb18dde690bc52a
Xuanhe Tian <xuanhe@txhsdell>
2023-10-09 11:07:20 +0100

    commit

--- frequencies.py
+++ frequencies.py
@@ -7,11 +7,8 @@ def frequencies(items):
         str_element = str(element)
         if str_element in frequencies:
             frequencies[str_element] = frequencies.get(str_element,0) +1 
-#
-frequencies[str_element] = frequencies.get(str_element,0) +1                                                           
 #dict[key] gives a value of the key ,  and we try to  use .get() to get the value of the element or key, 
 #if we can't get the value, we assign it o by default
-
     # Your code goes here
     return frequencies
 
