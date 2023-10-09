57e9b9ac2f05ef547ea97ef682813b7ec61c56b2
Xuanhe Tian <xuanhe@txhsdell>
2023-10-09 11:11:32 +0100

    a

--- frequencies.py
+++ frequencies.py
@@ -2,15 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {} # an empty array
-    for element in items:
-        str_element = str(element)
-        if str_element in frequencies:
-            frequencies[str_element] = frequencies.get(str_element,0) +1 
-#dict[key] gives a value of the key ,  and we try to  use .get() to get the value of the element or key, 
-#if we can't get the value, we assign it o by default
-    # Your code goes here
-    return frequencies
+    frequencies = {}  # Initialize an empty dictionary to hold the frequencies.
+    for element in items:  # Iterate through each item in the input list.
+        str_element = str(element)  # Convert the item to a string, ensuring keys are strings.
+        frequencies[str_element] = frequencies.get(str_element, 0) + 1  # Increment the count for each item (key).
+    return frequencies  # Return the dictionary containing the frequencies.
+
 
 
 
