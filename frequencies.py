df4498bbba229349242d16c5f31bfd820e8d5101
Xuanhe Tian <xuanhe@txhsdell>
2023-11-28 15:41:57 +0000

    modify

--- frequencies.py
+++ frequencies.py
@@ -5,11 +5,7 @@ def frequencies(items):
     frequencies = {} # an empty array
     for element in items:
         str_element = str(element)
-        if str_element in frequencies:
-            frequencies[str_element] = frequencies.get(str_element,0) +1 
-#dict[key] gives a value of the key ,  and we try to  use .get() to get the value of the element or key, 
-#if we can't get the value, we assign it o by default
-    # Your code goes here
+        frequencies[str_element] = frequencies.get(str_element,0) +1 
     return frequencies
 
 
